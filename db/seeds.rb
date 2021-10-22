251bfa29b11a2ef32c47f797d31b3946639920af
demeetra <71707122+demeetra@users.noreply.github.com>
2021-10-22 15:47:56 +0300

    russian translate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+puts "Destroyed everything you touch"
+posts = [
+{
+  author: "Nick Cave",
+  tag: "New Album",
+	title: "The Bad Seeds",
+	content: "Tracklist"
+},
+{
+  author: "Nick Cave",
+  tag: "New Album",
+	title: "The Bad Seeds",
+	content: "Tracklist"
+}
+]
+
+posts.each do |post|
+	postik = Post.create(post)
+	puts post
+	puts postik
+	puts postik.to_json
+	puts "Some magic just create a #{ postik.tag } with id #{ postik.id }!"
+end
