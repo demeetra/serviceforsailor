3cafc80dd31b0b3b7366c794c0b7bbe4314c414b
demeetra <71707122+demeetra@users.noreply.github.com>
2021-12-25 10:10:16 +0300

    nbvc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 		validates :author, :presence => true
-    # validates :tag, :presence => true
+    validates :tag, :presence => true
 		validates :title, :presence => true,
 											:length => { :minimum => 5 }
     has_many :comments, :dependent => :destroy
